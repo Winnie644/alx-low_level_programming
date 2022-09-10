76b186219601d63fc7893203ece5ea32d334a43b
winnie644 <fridahkemunto09@gmail.com>
2022-09-10 09:20:26 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- prints alphabets in lower case
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ar;
+
+	for (ar = 'a'; ar <= 'z'; ar++)
+	{
+	putchar(ar);
+	}
+	putchar('\n');
+	return (0);
+}
+
