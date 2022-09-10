74df7861019e02d3605391417b31f4f353624042
winnie644 <fridahkemunto09@gmail.com>
2022-09-10 05:56:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
