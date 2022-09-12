0363ea33284836522719e537b509275168b032b5
winnie644 <fridahkemunto09@gmail.com>
2022-09-12 12:19:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * Return: Always 1 (Success)
  */
 int main(void)
