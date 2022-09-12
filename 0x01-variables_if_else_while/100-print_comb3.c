efee0651dac47b49c3ecbe8e40d0e5fa933af85b
winnie644 <fridahkemunto09@gmail.com>
2022-09-12 13:49:37 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,27 +8,24 @@ int main(void)
 {
 	int n, m, l;
 /**/
-	for (n = 48; n <= 58; n++)
+	for (n = 0; n <= 8; n++)
 	{
-		for (m = 49; m <= 58; m++)
+		for (m = 0; m <= 9; m++)
 		{
-			for (l = 50; l < 58; l++)
+			if(n == 0 && m == 0)
 			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-
-					}
-				}
+				continue
 			}
+			putchar(n);
+			putchar(m);
+				if (m == 0 && n == 8)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
 		}
-	}
+	}	
 	putchar('\n');
 	return (0);
 }
