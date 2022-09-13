23c180a1cfd568248ff547bca6ce38d380f1c26b
winnie644 <fridahkemunto09@gmail.com>
2022-09-13 05:46:18 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-putchar.c
+ *
+ *
+ */
+
+#include "stdio.h"
+
+/**
+ * main - Prints "putchar", followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('s');
+	putchar('t');
+	putchar('d');
+	putchar('i');
+	putchar('o');
+	putchar('\n');
+	return (0);
+}
