76300a804e7b4b4cf3a19d28b24be9d4f24f1aa2
winnie644 <fridahkemunto09@gmail.com>
2022-09-20 07:33:32 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,42 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *  main - random password generator for 101-crackme
- *  Return: always 0
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
-
 int main(void)
 {
-	int i, j, k, s;
-
-	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char p[58];
+	int sum;
+	char a;
 
 	srand(time(NULL));
-	while (s != 2772)
+	while (sum <= 2645)
 	{
-		i = k = s = 0;
-		while ((2772 - 122) > s)
-		{
-			j = rand() % 62;
-			p[i] = c[j];
-			s += c[j];
-			i++;
-		}
-		while (c[k])
-		{
-			if (c[k] == (2772 - s))
-			{
-				p[i] = c[k];
-				s += c[k];
-				i++;
-				break;
-			}
-			k++;
-		}
+		a = rand() % 128;
+		sum += a;
+		putchar(a);
 	}
-	p[i] = '\0';
-	printf("%s", p);
+	putchar(2772 - sum);
 	return (0);
 }
