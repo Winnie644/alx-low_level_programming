2aeea9bed6f58e030ce8ac0afea7308209dca796
winnie644 <fridahkemunto09@gmail.com>
2022-09-21 06:30:08 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,24 +5,25 @@
  * @src: string to copy to dest
  * @n: number of bytes to copy from src
  *
- * Return: char pointer to beginning of dest
+ *Return: char pointer to beginning of dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *temp = dest;
 	int counter = 0, srcLength = 0;
 
-	while (*(src + srcLength) != '\0')
+	if (n <= 0)
+		return (dest);
+	while (src[srcLength] != '\0')
 		srcLength++;
-	while (*dest)
-		dest++;
+	srcLength++;
 	if (n > srcLength)
 		n = srcLength;
+
 	while (counter < n)
 	{
-		*dest++ = *src++;
+		dest[counter] = src[counter];
 		counter++;
 	}
-	*dest = '\0';
 	return (temp);
 }
