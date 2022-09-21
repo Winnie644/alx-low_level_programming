dc16dfcc4b728331e1294488164152d14ae9e1de
winnie644 <fridahkemunto09@gmail.com>
2022-09-21 13:05:24 -0700

    updated README.md

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,14 @@
+char get_leet(char);
+/**
+ * leet - transalte to leet speak
+ *
+ * @str: string to convert
+ *
+ *  Return: pointer to str
+ */
+char *leet(char *str)
+	char temp;
+	int i = 0;
+
+	while (str[i] != '\0')
+
