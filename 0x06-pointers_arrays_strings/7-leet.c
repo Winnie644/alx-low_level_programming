fca6060cc502dcb290d8be950df7e8b57bf1ee7a
winnie644 <fridahkemunto09@gmail.com>
2022-09-22 07:35:56 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,23 @@
-char get_leet(char);
-/**
- * leet - transalte to leet speak
- *
- * @str: string to convert
- *
- *  Return: pointer to str
- */
-char *leet(char *str)
-	char temp;
-	int i = 0;
+#include "main.h"
 
-	while (str[i] != '\0')
+/**
+ * leet - translates string to 'leet' (= 1337) language
+ * @s: input string to be translated
+ * Return: string after conversion of 'leet'
+ */
+
+char *leet(char *s)
+{
+	int a, b;
+
+	char letters[] = "aAeEoOtTlL";
+	char leet_it[] = "4433007711";
+
+	for (a = 0; s[a] != '\0'; a++)
+		for (b = 0; b < 10; b++)
+			if (s[a] == letters[b])
+				s[a] = leet_it[b];
+	return (s);
+}
+
 
