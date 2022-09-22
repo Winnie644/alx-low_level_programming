3a39261b33a1c5fb3aed3b4e160b2b60baa88346
winnie644 <fridahkemunto09@gmail.com>
2022-09-22 07:24:11 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,42 +1,18 @@
+#include "main.h"
+
 /**
- *  _strcmp - compares two strings
- *
- *   @s1: first string
- *   @s2: second string
- *
- *   Return: -15 if s1 < s2
- *            0  if s1 = s2
- *            15 if s1 > s2
+ * _strcmp - compares if 2 strings
+ *  @s1: destination string
+ *  @s2: source string to be copied
+ *  Return: difference of first characters that are of diff value
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int s1Length = 0, s2Length = 0;
-	int rtrnValNeg = -10;
-	int rtrnValPos = 10;
-	int smallerLength = 0;
+	char e;
 
-	while (*(s1 + s1Length) != '\0')
-		s1Length++;
-	while (*(s2 + s2Length) != '\0'
-			s2Length++;
-
-	smallerLength = (s1Length < s2Length) ? s1Length : s2Length;
-	rtrnValNeg += -(smallerLength);
-	rtrnValPos += smallerLength;
-
-	if (s1Length < s2Length)
-		return (rtrnValNeg);
-	if (s1Length > s2Length)
-		return (rtrnValPos);
-
-	while (*s1) /*same length, so loop through one and check both */
-	{
-		if (*s1 < *s2)
-			return (rtrnValNeg);
-		if (*s1 > *s2)
-		return (rtrnValPos);
-		s1++;
-		s2++;
-	}
-	return (0); /* equal */
+	for (e = 0; s1[e] != '\0' && s2[e] != '\0'; e++)
+		if (s1[e] != s2[e])
+			return (s1[e] - s2[e]);
+	return (0);
 }
