b11fd53ff70f43050668702becc39f8ce5649499
winnie644 <fridahkemunto09@gmail.com>
2022-09-22 11:30:51 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,8 +7,7 @@
  *
  * Return: char pointer to beginning of dest
  */
-char *
-strncpy(char *dest, char *src, int n)
+char * strncpy(char *dest, char *src, int n)
 {
 	char *temp = dest;
 	int counter = 0, srcLength = 0;
