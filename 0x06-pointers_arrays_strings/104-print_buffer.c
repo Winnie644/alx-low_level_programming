e63d51fea9f8fee5e437924b0f25d572b50d5bae
winnie644 <fridahkemunto09@gmail.com>
2022-09-23 09:16:29 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void print_hex_line(char *buffer, int numBitsInLine, int currentPosition);
 void print_buffer_line(char *b, int n, int cur);
 /**
  * print_buffer - prints contents of a buffer
@@ -30,6 +31,31 @@ void print_buffer(char *b, int size)
 		i++;
 	}
 }
+/**
+ * print_hex_line - prints chars of buffer as hex in sets of 2
+ *
+ * @b: buffer to print line from
+ * @numBitsInLine: number of bits in line, print spaces to fill in
+ * @currentPos: position in array of starting point of line
+ * Return: always void
+ *
+ */
+void print_hex_line(char *b, int numBitsInLine, int currentPos)
+{
+	int nestedCounter = 0;
+
+	while (nestedCounter < 10)
+	{
+		if (nestedCounter >= numBitsInLine)
+			printf("  ");
+		else
+			printf("%.2x", b[currentPos + nestedCounter]);
+		if (nestedCounter % 2)
+			putchar(' ');
+		nestedCounter++;
+	}
+}
+
 /**
  * print_buffer_line - prints chars of buffer as buffchar in sets of 2
  *
