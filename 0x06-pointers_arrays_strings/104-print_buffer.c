a54e7dba6ac0ad33a55d4f969f3c4bb1f4d2115e
winnie644 <fridahkemunto09@gmail.com>
2022-09-23 09:03:48 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-void print_hex_line(char *buffer, int numBitsInLine, int currentPosition);
-void print_buffer_line(char *b, int n, int cur);
 /**
  * print_buffer - prints contents of a buffer
- *
  * @b: buffer to print
  * @size: size of buffer
  *
  * Return: always void
  */
+
 void print_buffer(char *b, int size)
 {
 	int quo, rem, i = 0;
@@ -30,56 +27,5 @@ void print_buffer(char *b, int size)
 		putchar('\n');
 		bitCounter += 10;
 		i++;
-	}
-}
-/**
- * print_hex_line - prints chars of buffer as hex in sets of 2
- *
- * @b: buffer to print line from
- * @numBitsInLine: number of bits in line, print spaces to fill in
- * @currentPos: position in array of starting point of line
- *
- * Return: always void
- */
-void print_hex_line(char *b, int numBitsInLine, int currentPos)
-{
-	int nestedCounter = 0;
-
-	while (nestedCounter < 10)
-	{
-		if (nestedCounter >= numBitsInLine)
-			printf("  ");
-		else
-			printf("%.2x", b[currentPos + nestedCounter]);
-		if (nestedCounter % 2)
-			putchar(' ');
-		nestedCounter++;
-	}
-}
-/**
- * print_buffer_line - prints chars of buffer as buffchar in sets of 2
- *
- * @b: buffer to print line from
- * @numBitsInLine: number of bits in line, print spaces to fill in
- * @currentPos: position in array of starting point of line
- *
- * Return: always void
- */
-void print_buffer_line(char *b, int numBitsInLine, int currentPos)
-{
-	int nestedCounter = 0;
-
-	while (nestedCounter < 10)
-	{
-		if (nestedCounter >= numBitsInLine)
-			break;
-		else if (b[currentPos + nestedCounter] >= 32
-			&& b[currentPos + nestedCounter] < 127)
-		{
-			printf("%c", b[currentPos + nestedCounter]);
-		}
-		else
-			printf(".");
-		nestedCounter++;
 	}
 }
