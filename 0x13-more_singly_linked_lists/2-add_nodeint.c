2fe9adecfc4d1a6e6ead630542ad8239713b4ab9
winnie644 <fridahkemunto09@gmail.com>
2022-10-27 01:03:21 -0700

    2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_nodeint - adds a node of listint_t to the head of linked lists
+ * @head: pointer to a list_t pointer that points to the head struct
+ * @n: integer data to put in a new node
+ *
+ * Return: pointer to a new head list, NULL on failure
+ */
+listint_t add_nodeint(listint_t **head, cons int n)
+{
+	listint_t *temp_head;
+
+	temp_head = *head;
+	*head = malloc(sizeof(listint_t));
+	if (!*head)
+		return (NULL);
+	(*head)->n = n;
+	(*head)->next = temp_head;
+	return (*head);
+}
